b304aef26fdea547a2bbfa744a40b392974c347c
nayumayu <nayumayuchan@gmail.com>
2014-06-30 17:01:01 +0900

    Fix "__main__"

--- test.py
+++ test.py
@@ -8,34 +8,6 @@ from sklearn.neighbors import kneighbors_graph
 from autosp import predict_k
 
 
-def main(affinity_matrix):
-    """Test autosp
-
-    Parameters
-    ----------
-    affinity_matrix : array-like or sparse matrix, shape: (n_samples, n_samples)
-        adjacency matrix.
-        Each element of this matrix contains a measure of similarity between two of the data points.
-
-    Returns
-    ----------
-    labels: array of integers, shape: n_samples
-        The labels of the clusters.
-    k : integer
-        estimated number of cluster.
-    """
-
-    k = predict_k(affinity_matrix)
-
-    sc = SpectralClustering(n_clusters=k,
-                            affinity="precomputed",
-                            assign_labels="kmeans").fit(affinity_matrix)
-
-    labels = sc.labels_
-
-    return labels, k
-
-
 def color_map(labels):
     """Achieve "some" consistency of color between true labels and pred labels.
 
@@ -68,7 +40,7 @@ def color_map(labels):
 if __name__ == "__main__":
 
     # Generate artificial datasets.
-    number_of_blobs = 7  # You can change this!!
+    number_of_blobs = 5  # You can change this!!
     data, labels_true = make_blobs(n_samples=number_of_blobs * 10,
                                    centers=number_of_blobs)
 
@@ -76,10 +48,16 @@ if __name__ == "__main__":
     connectivity = kneighbors_graph(data, n_neighbors=10)
     affinity_matrix = 0.5 * (connectivity + connectivity.T)
 
-    labels_pred, k = main(affinity_matrix)
+    # auto_spectral_clustering
+    k = predict_k(affinity_matrix)
+    sc = SpectralClustering(n_clusters=k,
+                            affinity="precomputed",
+                            assign_labels="kmeans").fit(affinity_matrix)
 
     print("%d blobs(artificial datasets)." % number_of_blobs)
     print("%d clusters(predicted)." % k)
+
+    labels_pred = sc.labels_
 
     # Plot.
     from pylab import *
@@ -88,7 +66,6 @@ if __name__ == "__main__":
 
     p_map = color_map(labels_pred)
     p = [p_map[v] for v in labels_pred]
-
 
     subplot(211)
     title("%d blobs(artificial datasets)." % number_of_blobs)
